650d7066a59fb8c805a28ec54b20c33f1db4baff
DavidMurphy1088 <119924755+DavidMurphy1088@users.noreply.github.com>
2023-11-03 20:04:17 +1300

    1.31

--- MusicianshipTrainer/Models/TimeSlice.swift
+++ MusicianshipTrainer/Models/TimeSlice.swift
@@ -16,7 +16,7 @@ class TimeSlice : ScoreEntry {
     @Published var notesLength:Int?
     @Published var statusTag:StatusTag = .noTag
 
-    var score:Score?
+    var score:Score
     var footnote:String?
     var barLine:Int = 0
     private static var idIndex = 0
@@ -24,7 +24,7 @@ class TimeSlice : ScoreEntry {
     //Used when recording a tap sequence into a score
     var tapDuration:Double
     
-    init(score:Score?) {
+    init(score:Score) {
         self.score = score
         self.entries = []
         TimeSlice.idIndex += 1
@@ -47,32 +47,32 @@ class TimeSlice : ScoreEntry {
     func addNote(n:Note) {
         n.timeSlice = self
         self.entries.append(n)
-        if let score = score {
+        //if let score = score {
             let barAlreadyHasNote = score.noteCountForBar(pitch:n.midiNumber) > 1
             for i in 0..<score.staffs.count {
                 n.setNotePlacementAndAccidental(staff: score.staffs[i], barAlreadyHasNote: barAlreadyHasNote)
             }
             score.updateStaffs()
             score.addStemAndBeamCharaceteristics()
-        }        
+        //}
     }
     
     func addRest(rest:Rest) {
         self.entries.append(rest)
-        if let score = score {
+        //if let score = score {
             score.updateStaffs()
             score.addStemAndBeamCharaceteristics()
-        }
+        //}
     }
 
     func addChord(c:Chord) {
         for n in c.getNotes() {
             self.addNote(n: n)
         }
-        if let score = score {
+        //if let score = score {
             score.addStemAndBeamCharaceteristics()
             score.updateStaffs()
-        }
+        //}
     }
     
     func setTags(high:TagHigh, low:String) {
@@ -90,12 +90,12 @@ class TimeSlice : ScoreEntry {
         if getTimeSliceEntries().count == 0 {
             return
         }
-        if let score = score {
+        //if let score = score {
             let triad = score.key.makeTriadAt(timeSlice:timeSlice, rootMidi: rootNoteMidi, value: value, staffNum: staffNum)
             for note in triad {
                 addNote(n: note)
             }
-        }
+        //}
     }
     
     func anyNotesRotated() -> Bool {
